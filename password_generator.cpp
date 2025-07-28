e7d13c5e2b9af1d2f4afa34e98e54435dd80da2a
Harsh-Kapadiya <harsh2021800@gmail.com>
2025-07-28 20:14:44 +0530

    Update password_generator.cpp

--- password_generator.cpp
+++ password_generator.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 #include<ctime>
 #include<cstdlib>
+#include<string>
 using namespace std;
 int main(){
-    cout << "Your new protected password is :";
+    string password = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%&*()/*";
+    cout << "Your new protected password is: ";
+    srand(time(0));
+    for (int i=0; i < 16; i++){
+        cout << password[rand() % password.length()];
+    }
+    cout << endl;
     return 0;
 }
